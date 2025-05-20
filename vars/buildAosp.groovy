1c300ef7a4c5746007738a71f6d899fc9ac93d6d
Makson Lee <cdlee123@gmail.com>
2025-05-20 14:19:20 +0800

    feat(lib): migrate Gerrit SSH auth from Jenkins credential to Vault

--- vars/buildAosp.groovy
+++ vars/buildAosp.groovy
@@ -7,12 +7,22 @@ def call(Map config = [:]) {
     def lunchTarget = config.lunchTarget
 
     docker.image(dockerImage).inside("-v ${ccachePath}:/ccache") {
-        sshagent([sshCredential]) {
-            sh """#!/bin/bash
+        withVault([
+            vaultSecrets: [[
+                path: 'secret/jenkins/gerrit/maksonlee',
+                engineVersion: 2,
+                secretValues: [[envVar: 'GERRIT_SSH_KEY', vaultKey: 'ssh_key']]
+            ]]
+        ]) {
+            sh '''#!/bin/bash
                 mkdir -p ~/.ssh
                 chmod 700 ~/.ssh
+                echo "$GERRIT_SSH_KEY" > ~/.ssh/id_rsa
+                chmod 600 ~/.ssh/id_rsa
                 ssh-keyscan -p 29418 -H gerrit.maksonlee.com >> ~/.ssh/known_hosts
+            '''
 
+            sh """#!/bin/bash
                 repo init -u ${manifestUrl} -b ${branch}
                 repo sync -c -j1
 
